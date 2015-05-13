4fc491a1e2f3f1be1fa334487a106c02a22897cc
loevgaard <joachim@loevgaard.dk>
2015-05-13 15:13:21 +0200

    Updated Api.php with a new method

--- src/Dandomain/Api/Api.php
+++ src/Dandomain/Api/Api.php
@@ -5,31 +5,60 @@ class Api {
     protected $host;
     protected $apiKey;
     protected $query;
+    protected $debug = false;
 
     public function __construct($host, $apiKey) {
         $this->host = rtrim($host, '/');
         $this->apiKey = $apiKey;
     }
 
-    /**
-     * The equivalent to GetProduct
-     */
+    public function getOrders(\DateTime $dateStart, \DateTime $dateEnd) {
+        $this->query = "/admin/WEBAPI/Endpoints/v1_0/OrderService/{$this->apiKey}/GetByDateInterval?start=" . $dateStart->format('Y-m-d') . "&end=" . $dateEnd->format('Y-m-d');
+        return $this->run();
+    }
+    public function getOrdersInModifiedInterval(\DateTime $dateStart, \DateTime $dateEnd) {
+        $this->query = "/admin/WEBAPI/Endpoints/v1_0/OrderService/{$this->apiKey}/GetByModifiedInterval?start=" . $dateStart->format('Y-m-dTH:i:s') . "&end=" . $dateEnd->format('Y-m-dTH:i:s');
+        return $this->run();
+    }
     public function getProduct($productNumber, $siteId) {
         $this->query = "/admin/WEBAPI/Endpoints/v1_0/ProductService/{$this->apiKey}/" . rawurlencode($productNumber) . "/$siteId";
+        return $this->run();
+    }
+    public function getSites() {
+        $this->query = "/admin/WEBAPI/Endpoints/v1_0/SettingService/{$this->apiKey}/Sites";
         return $this->run();
     }
 
     protected function run() {
         $url = $this->host . $this->query;
+        if($this->debug) {
+            echo "URL: " . $url . "\n";
+        }
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 60);
         curl_setopt($ch, CURLOPT_TIMEOUT, 300);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array("Accept: application/json"));
         $content = curl_exec($ch);
+        $info = curl_getinfo($ch);
         curl_close($ch);
 
-        return $content;
+        if($info['http_code'] != 200) {
+            throw new \RuntimeException('HTTP ERROR', $info['http_code']);
+        }
+
+        return json_decode($content);
+    }
+
+    /**
+     * @param boolean $debug
+     * @return Api
+     */
+    public function setDebug($debug)
+    {
+        $this->debug = $debug;
+        return $this;
     }
 }
