7edd02308247c201888e3693199e1bd83955beb2
Joachim Løvgaard <joachim@loevgaard.dk>
2017-01-27 09:08:08 +0100

    Updated the jsonDateToDate function to accommodate time zones

--- src/Dandomain/Api/functions.php
+++ src/Dandomain/Api/functions.php
@@ -1,10 +1,31 @@
 <?php
 namespace Dandomain\Api;
 
-function jsonDateToDate($date) {
-    preg_match('/[0-9]{10,}/', $date, $matches);
-    if(!isset($matches[0])) {
+/**
+ * Return a \DateTime object based on a json string, i.e. '/Date(1484759471000+0100)/'
+ *
+ * @param $date
+ * @param string|\DateTimeZone $timeZone
+ * @return \DateTime
+ */
+function jsonDateToDate($date, $timeZone = null) {
+    preg_match('/([0-9]+)\+/', $date, $matches);
+    if(!isset($matches[1])) {
         throw new \InvalidArgumentException('$date is not a valid JSON date. Input: ' . $date);
     }
-    return new \DateTime('@' . substr($matches[0], 0, -3));
+    // remove the last three digits since the json date is given in milliseconds
+    $timestamp = substr($matches[1], 0, -3);
+
+    $dateTime = new \DateTime('@' . $timestamp, new \DateTimeZone('UTC'));
+
+    if($timeZone) {
+        if(is_string($timeZone)) {
+            $timeZone = new \DateTimeZone($timeZone);
+        }
+    } else {
+        $timeZone = new \DateTimeZone('Europe/Copenhagen');
+    }
+    $dateTime->setTimezone($timeZone);
+
+    return $dateTime;
 }
