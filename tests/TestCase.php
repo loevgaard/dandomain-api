7f4e14eafcfe972cd18e151af460794380d386e8
Joachim Løvgaard <joachim@loevgaard.dk>
2018-02-09 12:05:21 +0100

    fixed bug in abstract TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    protected function getApi(?ClientInterface $client = null) : Api
+    protected function getApi(ClientInterface $client = null) : Api
     {
         $api = new Api('https://example.com', 'apikeyapikeyapikeyapikeyapikeyapikey');
 
